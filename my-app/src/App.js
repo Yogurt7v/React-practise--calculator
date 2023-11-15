c43154b29d86859a4ec07f833bd6442920d8018d
Yogurt7v <egorov-2k@yandex.ru>
2023-11-15 19:29:47 +0300

    buttons added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,26 @@
 import './App.css';
 import React, { createContext, createElement } from 'react';
 
+const nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const ops = ['+', '-', '*', '/'];
 export const App = () => {
+	let startNum = 0;
 	return (
 		<>
 			<header className="App-header">Calculator</header>
 			<div className="Container">
-				<div className="Display">Display</div>
+				<div className="Display">{startNum}</div>
 				<div className="TwoBox">
-					<div className="Numbers">Numbers</div>
-					<div className="Operators">Operators</div>
+					<div className="Numbers">
+						{nums.map((num) => (
+							<button className="Number">{num}</button>
+						))}
+					</div>
+					<div className="Operators">
+						{ops.map((op) => (
+							<button className="Operator">{op}</button>
+						))}
+					</div>
 				</div>
 			</div>
 		</>
