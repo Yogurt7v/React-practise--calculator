c4c04bb37e421347106955520246beab75f6a245
Yogurt7v <egorov-2k@yandex.ru>
2023-11-16 22:59:45 +0300

    работает только один раз. дальше обнулять

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,68 +4,78 @@ import { useState } from 'react';
 
 const nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 const ops = ['+', '-', '*', '/'];
+let arr = [];
+let res; // можно и без этого
 export const App = () => {
-	let op = null;
 	let [displayNum, setDisplayNum] = useState(0);
 
-	const resetNumber = () => {
-		setDisplayNum(0);
-	};
-
-	const choseNumber = (event) => {
-		if (event.target.id === '') {
-			setDisplayNum(displayNum);
-		} else {
-			setDisplayNum(event.target.id);
+	const Result = (event) => {
+		if (event.target.id === 'Clear') {
+			setDisplayNum('0');
+			arr = [];
 		}
-	};
-
-	const choseOperator = (event) => {
-		op = event.target.id;
-		console.log(op);
-		// switch (event.target.id) {
-		// 	case '+':
-		// 		startNum ;
-		// 		break;
-		// 	case '-':
-		// 		setStartNum(startNum - event.target.id);
-		// 		break;
-		// 	case '*':
-		// 		setStartNum(startNum * event.target.id);
-		// 		break;
-		// 	case '/':
-		// 		setStartNum(startNum / event.target.id);
-		// 		break;
-		// }
+		if (nums.includes(event.target.id)) {
+			setDisplayNum(event.target.id);
+			arr.push(event.target.id);
+		}
+		if (ops.includes(event.target.id)) {
+			arr.push(event.target.id);
+		}
+		if (event.target.id === 'Calculate') {
+			switch (arr[1]) {
+				case '+':
+					res = Number(arr[0]) + Number(arr[2]);
+					break;
+				case '-':
+					res = Number(arr[0]) - Number(arr[2]);
+					break;
+				case '*':
+					res = Number(arr[0]) * Number(arr[2]);
+					break;
+				case '/':
+					res = Math.floor(Number(arr[0]) / Number(arr[2]));
+					break;
+				default:
+					setDisplayNum('Error');
+					break;
+			}
+			setDisplayNum(res);
+		}
 	};
 
 	return (
 		<>
-			<header className="App-header">Calculator</header>
+			<header className="App-header">
+				Работает только одно действие. После результата обнулять!!{' '}
+			</header>
 			<div className="Calculator">
-				<div className="Container">
+				<div className="Container" onClick={Result}>
 					<div className="Display">{displayNum}</div>
-					<div className="Clear" onClick={resetNumber}>
-						<button className="Clear">C</button>
+					<div className="Clear">
+						<button className="Clear" id="Clear">
+							Clear
+						</button>
 					</div>
 					<div className="TwoBox">
-						<div className="Numbers" onClick={choseNumber}>
+						<div className="Numbers">
 							{nums.map((num) => (
-								<button className="Number" id={num}>
+								<button className="Number" id={num} key={num}>
 									{num}
 								</button>
 							))}
 						</div>
-						<div className="Operators" onClick={choseOperator}>
+						<div className="Operators">
 							{ops.map((op) => (
-								<button className="Operator" id={op}>
+								<button className="Operator" id={op} key={op}>
 									{op}
 								</button>
 							))}
 						</div>
 					</div>
 					<div className="Result">
-						<button className="Result">=</button>
+						<button className="Result" onClick={Result} id="Calculate">
+							=
+						</button>
 					</div>
 				</div>
 			</div>
